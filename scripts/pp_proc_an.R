0eafd248e77bdb3072f359f869ed9488e0509c91
Akhila Nekkanti <akhilan@uoregon.edu>
2020-11-09 17:30:23 -0800

    factor outcome

--- scripts/pp_proc_an.R
+++ scripts/pp_proc_an.R
@@ -9,27 +9,31 @@ registerDoParallel(cl)
 foreach::getDoParWorkers()
 clusterEvalQ(cl, {library(tidymodels)})
 
-set.seed(3000)
-d <- read_csv("data/train.csv") %>% 
-  dplyr::sample_frac(.005)
 
+d <- read_csv("train.csv") #%>% 
+#  dplyr::sample_frac(.005)
+
+set.seed(3000)
 split <- initial_split(d)
 train <- training(split)
 train_cv <- vfold_cv(train)
 
 # basic recipe
 rec <- recipe(classification ~ stay_in_schl + tag_ed_fg + enrl_grd + ind_ed_fg, data = train)  %>% 
-  step_mutate(enrl_grd = as.factor(enrl_grd)) %>% 
+  step_mutate(enrl_grd = as.factor(enrl_grd),
+              classification = as.factor(classification)) %>% 
   step_unknown(all_nominal(), -all_outcomes())  %>% 
   step_novel(all_nominal(), -all_outcomes()) %>%
   step_nzv(all_predictors(), freq_cut = 0, unique_cut = 0) %>% 
   step_dummy(all_predictors(), -all_numeric(), -all_outcomes())  %>% 
   step_nzv(all_predictors()) 
 
+prep(rec)
+
 # linear regression model
-lm <- nearest_neighbor()  %>% 
-  set_mode("classification")  %>% 
-  set_engine("kknn")
+lm <- nearest_neighbor(neighbors = 11)  %>%
+  set_engine("kknn") %>% 
+  set_mode("classification")  
 
 fit1 <- fit_resamples(lm, rec, train_cv)
 saveRDS(fit1, "fit1.Rds")
