6414beca9714092216156de58e11738b59a39c35
Shaina Trevino <strevino@uoregon.edu>
2020-12-08 18:55:00 -0800

    adding info to compare models from rds

--- files for final/extract tuning parameters from rds.R
+++ files for final/extract tuning parameters from rds.R
@@ -13,17 +13,26 @@ rf_rds <- readRDS("model3-rf.Rds")
 #Extract spec model args
 
 knn_rds$.workflow
-#10 neighbors
+knn_rds %>%  
+  show_best(metric = "rmse")
+#10 neighbors, others default
 #1772.517 sec elapsed - from talapas
+#RMSE 92.2 - wow! - maybe because other default parameters esp. weight function
 
 # rf_rds$.workflow
 # 
 # options(max.print=1000000000)
 
 str(rf_rds$.workflow)
+rf_rds %>%  
+  show_best(metric = "rmse")
 #mtry = 5, min_n = 40, trees = 1000
 #13919.058 sec elapsed - from talapas
+#RMSE 85.8
 
 #linear reg results - from talapas .Rout
+lr_rds %>%  
+  show_best(metric = "rmse")
 # Penalty = 0.0000000001  Mixture = 0.0345
 #1552.898 sec elapsed
+#RMSE = 89.02
